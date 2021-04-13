ce356bcc07a94763f53179d7de546c12bb7733d7
Alexandre Bouvier <alexandre.b2506@gmail.com>
2021-04-13 17:35:19 +0200

    Finish exercice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  get 'pets/new', to: 'pets#new', as: :new_pet
   get 'pets', to: 'pets#index'
-  get 'pets/:id', to: 'pets#show'
+  get 'pets/:id', to: 'pets#show', as: :pet
+  post 'pets', to: 'pets#create'
+  delete 'pets/:id', to: 'pets#destroy'
+  patch 'pets/:id', to: 'pets#update'
+
+  # resources :pets
 end
